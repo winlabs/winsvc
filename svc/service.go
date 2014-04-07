3894cd0b47a853ba5a8c4f7926de281530afa6f8
Alex Brainman <alex.brainman@gmail.com>
2014-04-07 16:43:51 +1000

    winsvc: add +build windows tag to everything
    Fixes issue 3
    
    LGTM=matt.jibson
    R=alex.brainman, matt.jibson
    CC=brainman
    https://codereview.appspot.com/84360044

--- svc/service.go
+++ svc/service.go
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// +build windows
+
 // Package svc provides everything required to build Windows service.
 //
 package svc
